4aae53c5ffc94392bdceaaf6fd58391a94197133
leixiang <leixiang@mogulinker.com>
2023-03-07 17:56:24 +0800

    first push

---
+++ workflow.py
@@ -0,0 +1,22 @@
+%%writefile {project_dir}/workflow.py
+from kfp import dsl
+from mlrun import mount_v3io
+
+funcs = {}
+
+def init_functions(functions: dict, project=None, secrets=None):
+    functions['ingest'].apply(mount_v3io())
+
+@dsl.pipeline(
+    name='demo project', description='Shows how to use mlrun project.'
+)
+def kfpipeline(p1=3):
+    # first step build the function container
+    builder = funcs['tstfunc'].deploy_step(with_mlrun=False)
+    
+    ingest = funcs['ingest'].as_step(name='load-data', params={'dataset': 'boston'})
+
+    # first step
+    s1 = funcs['tstfunc'].as_step(name='step-one', handler='my_func',
+         image=builder.outputs['image'],
+         params={'p1': p1})
